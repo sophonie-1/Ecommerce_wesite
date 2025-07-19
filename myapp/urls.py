e4f1bd0eef26db2240e72891bd1d9010b0b8787a
bukira <bukirasophonie347@gmail.com>
2025-07-19 16:09:56 +0300

    templates theme: i have created user interface likes styles of all pages need

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from .views import store_view, cart_view, checkout_view
 
-
+app_name = 'myapp'
 urlpatterns = [
     path('', store_view, name='store'),
     path('cart/', cart_view, name='cart'),
