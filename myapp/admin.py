1e4d1a1136f06eba31dc29ce52fa8a4eafa4323e
bukira <bukirasophonie347@gmail.com>
2025-07-19 17:01:08 +0300

    models or database: i have set database table

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+from .models import CustomerProfile, Product, Order, OrderItem, ShippingAddress
 # Register your models here.
+
+
+admin.site.register([
+    CustomerProfile,
+    Product,
+    Order,
+    OrderItem,
+    ShippingAddress
+])
--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,69 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+
+class CustomerProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    email = models.EmailField(max_length=254, unique=True)
+    phone_number = models.CharField(max_length=15, blank=True, null=True)
+    address = models.TextField(blank=True, null=True)
+    customer_image = models.ImageField(upload_to='customer_images/', blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    date_of_birth = models.DateField(blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.user.username}'s Profile"
+    
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    digital = models.BooleanField(default=False)
+    image = models.ImageField(upload_to='products/', blank=True, null=True)
+    stock = models.PositiveIntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+    
+class Order(models.Model):
+    status_choices = (
+        ('Pending', 'Pending'),
+        ('Processing', 'Processing'),
+        ('Completed', 'Completed'),
+        ('Cancelled', 'Cancelled'),
+    )
+    customer = models.ForeignKey(CustomerProfile, on_delete=models.SET_NULL ,null=True)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    order_date = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=20, choices=status_choices, default='Pending')
+    completed = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"Order {self.id} by {self.customer.user.username}"
+    
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, related_name='items', on_delete=models.SET_NULL,null=True)
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL,null=True)
+    quantity = models.PositiveIntegerField(default=0)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.quantity} x {self.product.name} in Order {self.order.id}"
+    
+class ShippingAddress(models.Model):
+    customer = models.ForeignKey(CustomerProfile, on_delete=models.SET_NULL, null=True)
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
+    address_line1= models.CharField(max_length=255, blank=True, null=True)
+    city = models.CharField(max_length=100)
+    state = models.CharField(max_length=100)
+    zip_code = models.CharField(max_length=20)
+    country = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Shipping Address for Order {self.order.id} by {self.customer.user.username}"
